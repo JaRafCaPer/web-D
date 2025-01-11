c47153bd3c11759301cf9fd11860bbc1139a94be
Javier Caballero <caballeroperezjavier@gmail.com>
2025-01-11 15:05:13 -0500

    Fixing parallax

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import "./Home.css";
@@ -10,18 +10,20 @@ import Form from "../../components/Form/ContactForm";
 import ServicesCards from "../../components/ServicesCards/ServicesCards";
 
 function Home() {
-  const backgroundRef = useRef(null);
-
   const instagramEmbedCode = `
     <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/p/C2Pj7hYgaOW/?igsh=MTZ5bGpmaWZscG9uMA==" data-instgrm-version="14" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:640px; min-width:426px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
       <div class="instPost"> 
-        <a href="https://www.instagram.com/p/C2Pj7hYgaOW/?igsh=MTZ5bGpmaWZscG9uMA==" target="_blank"></a>
+        <a href="https://www.instagram.com/p/C2Pj7hYgaOW/?igsh=MTZ5bGpmaWZscG9uMA==" className="instA"  target="_blank"> 
+        </a>
       </div>
     </blockquote>
   `;
 
   useEffect(() => {
-    AOS.init({ duration: 2200 });
+    AOS.init({
+      duration: 2200,
+      
+    });
 
     const script = document.createElement("script");
     script.async = true;
@@ -39,48 +41,39 @@ function Home() {
     };
   }, []);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (backgroundRef.current) {
-        const scrollY = window.scrollY;
-        backgroundRef.current.style.transform = `translateY(${scrollY * 0.15}px)`;
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <>
-      <div className="dynamic-background" ref={backgroundRef}></div>
+    <><div className="sticky">
+    <div className="parallax"></div>
+    </div>
       <div className="content">
         <div className="parent">
           <div className="div1" data-aos="fade-up">
             <div className="InDiv1">
-              <h1>¡Tu Tranquilidad, Mi Compromiso!</h1>
-              <div
-                className="instagram-embed"
-                dangerouslySetInnerHTML={{ __html: instagramEmbedCode }}
-              />
+              <h1>
+                ¡Tu Tranquilidad, Mi compromiso!
+              </h1>
+              
+              <div className="instagram-embed" dangerouslySetInnerHTML={{ __html: instagramEmbedCode }} />
             </div>
           </div>
           <div className="div2" data-aos="fade-right">
-            <img src={denkoImage} alt="Denko Swoboda" className="denkoImage" />
+            <img
+              src={denkoImage}
+              alt="Denko Swoboda"
+              className="denkoImage"
+            />
           </div>
         </div>
-        <HeroSection />
+        <HeroSection  />
         <div className="div3" data-aos="fade-down">
           <div className="subDiv3">
-            <img src={familyImage} alt="Family" />
+            <img src={familyImage} alt="" />
             <p className="pieFoto">Foto de Liam Anderson</p>
           </div>
           <Form className="formulario" />
         </div>
         <div data-aos="fade-right">
-          <ServicesCards />
+        <ServicesCards />
         </div>
       </div>
     </>
